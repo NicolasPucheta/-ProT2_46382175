e5dd8ba1bbe43ffb6db5fbe36dca0116a20a6b8e
NicolasPucheta <nicopuche112005@gmail.com>
2025-05-28 03:02:29 -0300

    principal completado con estilo elegido

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,9 +6,39 @@ class Home extends BaseController
 {
     public function index()
     {
-        echo view('front/head_view');
+         $data['Titulo'] = 'Eclat Perfumes';
+        echo view('front/head_view',$data);
         echo view('front/navbar');
-        echo view('principal');
+        echo view('front/principal');
         echo view('front/footer_view');
+    }
+
+    
+    public function quienesSomos()
+    {
+        $data['Titulo'] = 'Quienes Somos';
+        echo view('front/head_view',$data);
+        echo view('front/navbar',$data);
+        echo view('front/Quienes_Somos');
+        echo view('front/footer_view');
+    }
+
+    public function Productos()
+    {
+        $data['Titulo'] = 'Productos';
+        echo view('front/head_view',$data);
+        echo view('front/navbar',$data);
+        echo view('front/productos',$data);
+        echo view('front/footer_view',$data);
+    }
+
+    public function login()
+    {
+        $data['Titulo'] = 'Login';
+        echo view('front/head_view', $data);
+        echo view('front/navbar',$data);
+        echo view('front/inicio_sesion', $data); 
+        echo view('front/footer_view',$data );
+
     }
 }
